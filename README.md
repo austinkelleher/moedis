3a50c8f53b13a15c2923954a9b96f8c6b02f9c9f
austinkelleher <austin.kell47@gmail.com>
2016-02-08 15:19:51 -0500

    Initial moedis commit.

--- README.md
+++ README.md
@@ -1,2 +1,61 @@
-# moedis
-Cache Mongoose model properties in Redis
+moedis
+===========
+Cache Mongoose model properties in Redis.
+
+## Installation
+```bash
+npm install moedis --save
+```
+
+## Requiring
+```javascript
+var Moedis = require('moedis');
+```
+
+## Usage
+
+```javascript
+var Moedis = require('moedis');
+var mongoose = require('mongoose');
+var redis = require('redis');
+
+var redisClient = redis.createClient();
+
+var UserSchema = mongoose.Schema({
+    email: String,
+    password: String
+});
+
+var User = new Moedis({
+    modelName: 'User',
+    schema: UserSchema,
+    redis: redisClient,
+    mongoose mongoose
+});
+
+var user = new User({
+    email: 'test@test.com',
+    password: 'test'
+});
+
+// Optional callback as third argument
+user.set('email', user.password, function(err, reply) {
+    // reply contains the reply from Redis
+    if (err) {
+        console.log('Error setting property: ', err);
+        return;
+    }
+
+    user.get('email', function(err, reply) {
+        if (err) {
+            console.log('Error retrieving property: ', err);
+            return;
+        }
+
+        // reply should be the password!
+    });
+});
+
+
+
+```
---
+++ index.js
@@ -0,0 +1,69 @@
+/**
+* Generates a normalized Cache key for Redis
+* @param modelName - Name of the Mongoose model
+* @param prop - Property to cache
+* @param entity - Value of the property (prop)
+*/
+function getCacheKey(modelName, prop, entity) {
+    return modelName + '-' + prop + ':' + entity;
+}
+
+module.exports = function(options) {
+    var modelName = options.modelName;
+    var schema = options.schema;
+    var redis = options.redis;
+    var mongoose = options.mongoose;
+
+    if (!modelName || !schema || !redis || !mongoose) {
+        throw new Error('Required argument missing!');
+    }
+
+    /**
+    * Cache a Mongoose model property into Redis
+    * @param prop {string} - Mongoose schema property to cache
+    * @param value {string} - Value to set
+    * @param callback
+    */
+    schema.methods.set = function(prop, value, callback) {
+        var entity = this[prop];
+
+        if (!entity) {
+            throw new Error('Invalid property or undefined value.');
+        }
+
+        var key = getCacheKey(modelName, prop, entity);
+
+        if (callback) {
+            redis.set(key, value, function(err, reply) {
+                callback(err, reply);
+            });
+        } else {
+            redis.set(key, value);
+        }
+    };
+
+    /**
+    * Fetch a cached property from Redis
+    * @param prop {string} - Mongoose schema property to fetch from cache
+    * @param callback {function}
+    */
+    schema.methods.get = function(prop, callback) {
+        var entity = this[prop];
+
+        if (!entity) {
+            throw new Error('Invalid property or undefined value.');
+        }
+
+        var key = getCacheKey(modelName, prop, entity);
+
+        if (callback) {
+            redis.get(key, function(err, reply) {
+                callback(err, reply);
+            });
+        } else {
+            redis.get(key);
+        }
+    };
+
+    return mongoose.model(modelName, schema);
+};
